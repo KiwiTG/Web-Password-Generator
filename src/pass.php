8e53209f99564b9c6f59745af09ed9de8b936752
H4Z3 <93849885+KiwiTG@users.noreply.github.com>
2022-10-27 12:37:35 -0400

    Update pass.php

--- src/pass.php
+++ src/pass.php
@@ -10,6 +10,20 @@ body {
 <title>PassGen - H4Z3</title>
 
 <?php
+$passCount = fopen("genCount.txt", "r");
+$gnRead = fread($passCount,filesize("genCount.txt"));
+file_put_contents("genCount.txt", $gnRead + 1);
+fclose($passCount);
+?>
+
+<?php
+$gc = fopen("genCount.txt", "r");
+$gcr = fread($gc,filesize("genCount.txt"));
+echo nl2br("<font color=white>\nPasswords Generated: <font color=green>".$gcr);
+echo nl2br("<font color=white>\n\n")
+?>
+
+<?php
 $n=$_POST["plen"];
 
 function secPass($n) {
@@ -29,6 +43,6 @@ echo secPass($n);
 echo "<font color=white>"
 ?>
 
-<br><br><form method="POST" action="/">
+<br><form method="POST" action="/">
     <input type="submit" value="Return"/>
 </form>
